d1146deb3e08890f0969c6d2d2c9da0fed724a33
Asker_samurai <asker_original@mail.ru>
2023-10-23 15:41:09 +0300

    drf

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,13 @@
+from datetime import date
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.DecimalField(max_digits=4, decimal_places=1, verbose_name='Температура')
+    created_at = models.DateField(default=date.today, verbose_name='Дата измерения')
