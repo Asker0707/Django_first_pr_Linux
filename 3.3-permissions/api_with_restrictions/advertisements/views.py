71a83ceaee4459413fd9d0f1ba6a3651831374ac
Asker_samurai <asker_original@mail.ru>
2023-12-17 19:14:44 +0300

    api_restriction

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,13 +1,16 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+from advertisements.permissions import IsOwnerOrReadOnly
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-
+    queryset = Advertisement.objects.all()
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
-
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update"]:
